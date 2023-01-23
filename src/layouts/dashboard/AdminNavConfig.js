aaec464a2270539ad5389e4e2267017d1e8dbd3f
thePikture <infra@thepikture.com>
2023-01-23 14:46:37 +0530

    Icons:Ak

--- src/layouts/dashboard/AdminNavConfig.js
+++ src/layouts/dashboard/AdminNavConfig.js
@@ -12,9 +12,44 @@ const adminNavConfig = [
         icon: getIcon('material-symbols:dashboard-rounded'),
     },
     {
-        title: 'Transaction',
-        path: '/dashboard/transaction',
-        icon: getIcon('grommet-icons:transaction'),
+        title: 'Event',
+        path: '',
+        icon: getIcon('mdi:events'),
+    },
+    {
+        title: 'Warnln',
+        path: '',
+        icon: getIcon('simple-icons:eventstore'),
+    },
+    {
+        title: 'Members',
+        path: '',
+        icon: getIcon('ic:outline-card-membership'),
+    },
+    {
+        title: 'Circulars and rules',
+        path: '',
+        icon: getIcon('mdi:circular-arrows'),
+    },
+    {
+        title: 'Newslettters',
+        path: '',
+        icon: getIcon('fluent:news-20-regular'),
+    },
+    {
+        title: 'Room bookings',
+        path: '',
+        icon: getIcon('tabler:brand-booking'),
+    },
+    {
+        title: 'Messages',
+        path: '',
+        icon: getIcon('simple-icons:googlemessages'),
+    },
+    {
+        title: 'Administration',
+        path: '',
+        icon: getIcon('medical-icon:administration'),
     },
 ];
 
